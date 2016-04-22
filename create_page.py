4dfc28b15d54fad79bd0a79735185148e7e0eee5
Josua Krause <josua.krause@gmail.com>
2016-04-22 14:11:10 -0400

    utf?

--- create_page.py
+++ create_page.py
@@ -113,7 +113,7 @@ def create_media(pref, types, docs, dry_run):
         if not os.path.exists(os.path.dirname(timeline_fn)):
             os.makedirs(os.path.dirname(timeline_fn))
         with io.open(timeline_fn, 'w', encoding='utf8') as tl:
-            json.dump({ "events": events }, tl, sort_keys=True, indent=2)
+            print(json.dumps({ "events": events }, sort_keys=True, indent=2).encoding('utf8'), file=tl)
     return content
 
 def apply_template(tmpl, docs, pref, dry_run):
