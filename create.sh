4e3da021bc9358d7ba77749c26fd204cd2547402
Josua Krause <josua.krause@gmail.com>
2016-05-06 23:13:27 -0400

    use build script in travis

--- create.sh
+++ create.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -ex
 
-OUTPUT="www"
-LIB_COPY="filelist.txt"
-NO_WEB=1
-PUBLISH=
+if [ -z $NO_DEFAULT ]; then
+  OUTPUT="www"
+  LIB_COPY="filelist.txt"
+  NO_WEB=1
+  PUBLISH=
+fi
+
 
 if [ -z $NO_WEB ]; then
   git submodule update --init --recursive
@@ -18,7 +21,7 @@ rsync -av . "${OUTPUT}" --files-from "${LIB_COPY}"
 ./create_page.py --documents content.json --template index.tmpl --out "${OUTPUT}/index.html" --prefix "${OUTPUT}"
 
 if [ -z $PUBLISH ]; then
-  python -m SimpleHTTPServer || echo ""
+  python -m SimpleHTTPServer || true
   rm -rf "${OUTPUT}"
   rm "${LIB_COPY}"
 else
