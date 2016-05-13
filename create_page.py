a0de8cddb2142ea5c09d66671adc70a2e04c01ed
Josua Krause <josua.krause@gmail.com>
2016-05-13 09:42:08 -0700

    track links

--- create_page.py
+++ create_page.py
@@ -22,6 +22,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-77732102-1', 'auto');
+ga('require', 'linkid');
 ga('send', 'pageview');
 """
 
