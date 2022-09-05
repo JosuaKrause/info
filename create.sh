8429dc251c0da6935bf42a7e1392042cd25783e0
Josua Krause <joschi@accern.com>
2022-09-05 00:03:40 -0400

    more

--- create.sh
+++ create.sh
@@ -19,6 +19,7 @@ find . -not -path '*.git/*' -not -path '*'"${OUTPUT}/"'*' -not -path '*mvn-repo/
 rsync -av . "${OUTPUT}" --files-from "${LIB_COPY}"
 python create_page.py --documents content.json --template index.tmpl --out "${OUTPUT}/index.html" --prefix "${OUTPUT}"
 PREV_DIR=`pwd`
+pushd "${OUTPUT}" && find . -not -path '*lib/*' -not -path '*lib' && popd
 pushd "${OUTPUT}" && find . -not -path '*lib/*' -not -path '*lib' | python "${PREV_DIR}/create_sitemap.py" "sitemap.xml" && popd
 
 if [ -z $PUBLISH ]; then
