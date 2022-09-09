9d2a34cc1c0db194fb60b1b895a6b882a6e3ffa6
Josua Krause <joschi@accern.com>
2022-09-09 09:41:27 -0400

    update ga

--- create_page.py
+++ create_page.py
@@ -86,14 +86,16 @@ LD_JSON_KNOWLEDGE = """{
 }"""
 
 GA_TRACKING = """
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-77732102-1">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-ga('create', 'UA-77732102-1', 'auto');
-ga('require', 'linkid');
-ga('send', 'pageview');
+  gtag('config', 'UA-77732102-1');
+</script>
 """
 
 TAG = r"<[^>]*?>"
